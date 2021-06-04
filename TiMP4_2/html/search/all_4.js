10522a24dcd8b5f868e84f2ad86b99ba118791b2
SullenFox <81508667+SullenFox@users.noreply.github.com>
2021-06-04 19:28:08 +0300

    Add files via upload

---
+++ TiMP4_2/html/search/all_4.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['task_5f2_2ecpp_4',['Task_2.cpp',['../Task__2_8cpp.html',1,'']]]
+];
---
+++ TiMP4_2/html/search/files_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['task_5f2_2ecpp_7',['Task_2.cpp',['../Task__2_8cpp.html',1,'']]]
+];
